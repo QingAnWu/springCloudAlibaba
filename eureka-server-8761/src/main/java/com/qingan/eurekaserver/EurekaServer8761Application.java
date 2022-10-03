c71a1e6be6ea13932638073de5d8edb6f09aebd4
QingAn <571206595@qq.com>
2022-10-03 23:48:55 +0800

    eureka

--- eureka-server-8761/src/main/java/com/qingan/eurekaserver/EurekaServer8761Application.java
+++ eureka-server-8761/src/main/java/com/qingan/eurekaserver/EurekaServer8761Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class EurekaServer8761Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(EurekaServer8761Application.class, args);
     }
 
 }
