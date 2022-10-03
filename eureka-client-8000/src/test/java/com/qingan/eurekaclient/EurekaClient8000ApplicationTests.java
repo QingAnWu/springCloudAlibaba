847db2be44e47244055879fa40fd796d61ba9cd0
QingAn <571206595@qq.com>
2022-10-03 10:40:14 +0800

    Initial commit

---
+++ eureka-client-8000/src/test/java/com/qingan/eurekaclient/EurekaClient8000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qingan.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClient8000ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
